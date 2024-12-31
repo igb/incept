d7f28576d595ff46333df3b99c375851f4dec982
Ian Brown <ibrown@netflix.com>
2024-12-31 07:50:09 -0800

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,28 @@
 use evdev::{Device, InputEventKind};
 use std::path::Path;
 
-
 fn main() {
-
-
     let device_path = Path::new("/dev/input/event3");
 
-
-  // Open the input device
+    // Open the input device
     let mut device = match Device::open(device_path) {
         Ok(dev) => dev,
         Err(err) => {
             eprintln!("Failed to open device: {}", err);
             return;
-        },
+        }
     };
-    
-    println!("Listening for events on: {}", device.name().unwrap_or("Unknown device"));
-    
-    
+
+    println!(
+        "Listening for events on: {}",
+        device.name().unwrap_or("Unknown device")
+    );
+
     // Event loop: read and process events
     loop {
-    	for ev in device.fetch_events().unwrap() {
+        for ev in device.fetch_events().unwrap() {
             match ev.kind() {
-	      	InputEventKind::Key(key) => {
+                InputEventKind::Key(key) => {
                     println!("Key event: {:?} - State: {}", key, ev.value());
                 }
                 InputEventKind::RelAxis(axis) => {
@@ -33,12 +31,10 @@ fn main() {
                 InputEventKind::AbsAxis(axis) => {
                     println!("Absolute axis event: {:?} - Value: {}", axis, ev.value());
                 }
-           	_ => {
-		    println!("Other event: {:?}", ev);
+                _ => {
+                    println!("Other event: {:?}", ev);
                 }
             }
         }
     }
-    
-    
 }
